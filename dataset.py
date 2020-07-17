34363db153306b3f54ff94178d7d3067907d2443
Jathin Pranav Singaraju <66273559+DaOne23-JPS@users.noreply.github.com>
2020-07-17 15:20:13 -0500

    Pranav - Compress Images
    Tried to compress images didn't work yet.

--- dataset.py
+++ dataset.py
@@ -1,7 +1,11 @@
-import os
-import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import load_img
+import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 # Pranav directory -
 noMaskDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/without_mask/'
@@ -9,17 +13,23 @@ maskDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/with_ma
 
 # Lavik directory -
 # nothing now
-# nothing now
 
-maskSet = noMaskSet = np.array([])
+maskSet = noMaskSet = data = np.array([])
 
+counter = 0
 for image in os.listdir(noMaskDir):
-    noMaskSet = np.append(noMaskSet, noMaskDir + image)
-for image in os.listdir(maskDir):
-    maskSet = np.append(maskSet, maskDir + image)
+    counter += 1
+    print(counter)
+    imageMain = mpimg.imread(noMaskDir + image)
+    noMaskSet = np.append(noMaskSet, imageMain)
+    # image = load_img(imgPath, color_mode="grayscale", target_size=(256, 256, 1))
+    # imageMain = img_to_array(image)
+
+# maskSet = np.array(maskSet, dtype='float32')
+# noMaskSet = np.array(noMaskSet, dtype='float32')
 
 # show the image if needed
-for image in maskSet:
-    imageMain = mpimg.imread(image)
-    plt.imshow(imageMain)
-    plt.show()
+# for image in maskSet:
+#     imageMain = mpimg.imread(image)
+#     plt.imshow(imageMain)
+#     plt.show()
