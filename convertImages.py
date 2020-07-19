bafe645be2df09cf143c3aec1745c7190adfdb72
Jathin Pranav Singaraju <66273559+DaOne23-JPS@users.noreply.github.com>
2020-07-18 23:52:54 -0500

    Pranav - Bar Update and Read Images
    I added a bar method so we could track progress on the first 10k images with a mask. Still need to implement it in the second set of 10k images. It now reads all 20k images in only 1 minute.

--- convertImages.py
+++ convertImages.py
@@ -1,3 +1,4 @@
+# this file only needs to be run once
 import os
 import cv2
 import time
@@ -16,12 +17,10 @@ import time
 withoutDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/without_mask/'
 withDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/new_with_mask/'
 newWithoutDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/new_without_mask/'
-newWithDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/new_mask/'
+newWithDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/new_with_mask/'
 
-# initialize the follwoing variables
+# initialize the time variable
 totalTime = 0
-noMaskSet = []
-maskSet = []
 
 # set the folder you want to iterate through, in this case withDir (rgb images with masks)
 for image in os.listdir(withDir):
@@ -38,7 +37,21 @@ for image in os.listdir(withDir):
     totalTime += endTime - startTime  # add total time
     print(totalTime)  # print the time after each iteration
 
+# the other folder to iterate through, withoutDir (rgb without masks), same code, different directory
+for image in os.listdir(withoutDir):
+    startTime = time.time()  # start the time
+    print(image)  # print the image name
+    imageMain = cv2.imread(withoutDir + image)  # read the image from the directory specified
+    scaleFactor = 25  # set the scale factor to 25% of the original image
+    # find the new dimensions using the scaleFactor
+    dim = int(imageMain.shape[1] * scaleFactor / 100), int(imageMain.shape[0] * scaleFactor / 100)
+    imageMain = cv2.resize(imageMain, dim)  # resize the images with specs
+    imageMain = cv2.cvtColor(imageMain, cv2.COLOR_BGR2GRAY)  # make the image grayscale
+    cv2.imwrite(newWithoutDir + image, imageMain)  # write the image, or save it into the new folder (newWithDir)
+    endTime = time.time()  # end the time
+    totalTime += endTime - startTime  # add total time
+    print(totalTime)  # print the time after each iteration
+
 # code to show image with array = imageMain
 # plt.imshow(imageMain)
 # plt.show()
-# noMaskSet.append(imageMain)
--- pranavCode.py
+++ pranavCode.py
@@ -1,14 +1,26 @@
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.preprocessing.image import load_img
-from PIL import Image
-import numpy as np
 import os
 import cv2
-import time
+import progressBar
 
-# Pranav code
-# nothing yet
+newWithoutDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/new_without_mask/'
+newWithDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/new_with_mask/'
+
+noMaskSet = []
+maskSet = []
+counter = 0
+i = 0
+
+# bar method with reading the image for the 10k images with a mask
+progressBar.barMethod1(0, 1000, prefix='Loading Faces... ', suffix='Complete', length=50)
+for image in os.listdir(newWithDir):
+    if counter % 10 == 0:
+        progressBar.barMethod1(i + 1, 1000, prefix='Loading Faces... ', suffix='Complete', length=50)
+        i += 1
+    imageMain = cv2.imread(newWithDir + image)
+    maskSet.append(imageMain)
+    counter += 1
+
+# second bar code didn't do yet, still need to finish that
+for image in os.listdir(newWithoutDir):
+    imageMain = cv2.imread(newWithoutDir + image)
+    noMaskSet.append(imageMain)
