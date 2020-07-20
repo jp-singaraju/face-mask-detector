47ba308584c5050f12a6af04fc5a88aa171b24d8
Jathin Pranav Singaraju <66273559+DaOne23-JPS@users.noreply.github.com>
2020-07-20 16:53:41 -0500

    Pranav - Tried Working on the Model
    I tried to work on the model, but I didn't get anywhere. Added a couple of files.

--- pranavCode.py
+++ pranavCode.py
@@ -1,12 +1,52 @@
-import matplotlib.image as mpimg
-import matplotlib.pyplot as plt
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.preprocessing.image import load_img
-from PIL import Image
-import numpy as np
-import os
+from imutils.video import VideoStream
+import time
+import argparse
+import imutils
+import time
 import cv2
+import os
+import numpy as np
 
-# nothing now
+ap = argparse.ArgumentParser()
+ap.add_argument("-f", "--face", type=str,
+                default="face_detector",
+                help="path to face detector model directory")
+ap.add_argument("-m", "--model", type=str,
+                default="mask_detector.model",
+                help="path to trained face mask detector model")
+args = vars(ap.parse_args())
+
+prototxtPath = os.path.sep.join([args["face"], "deploy.prototxt.txt"])
+weightsPath = os.path.sep.join([args["face"],
+                                "res10_300x300_ssd_iter_140000.caffemodel"])
+net = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+
+def myImage(img):
+    image = cv2.imread(img)
+    # construct a blob from the image
+    blob = cv2.dnn.blobFromImage(image, 1.0, (300, 300), (104.0, 177.0, 123.0))
+    # pass the blob through the network and obtain the face detections
+    print("[INFO] computing face detections...")
+    net.setInput(blob)
+    detections = net.forward()
+
+
+# initialize the video stream and allow the camera sensor to warm up
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+
+frame = vs.read()
+
+key = 'a'
+
+while key != ord('q'):
+    frame = vs.read()
+    # show the output frame
+    myImage(frame)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+cv2.destroyAllWindows()
+vs.stop()
