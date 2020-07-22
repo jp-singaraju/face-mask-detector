fa3c7f7a3c0fff2e8ceea9617b0fa3c8310814c3
lavikj <lavikjain@gmail.com>
2020-07-22 18:51:08 -0500

    Lavik - Tensorflow Troubleshooting
    I just changed a little bit as most of the time was spent troubleshooting

--- modelTest.py
+++ modelTest.py
@@ -1,8 +1,6 @@
-# from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, AveragePooling2D, Flatten, Dropout
+from tensorflow.keras.layers import Dense, AveragePooling2D, Flatten, Dropout, Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.layers import Input
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from main import finalSet
@@ -26,36 +24,28 @@ INIT_LR = 1e-4
 EPOCHS = 30
 BS = 32
 
-baseModel = MobileNetV2(input_shape=(256, 256, 3), weights='imagenet', include_top=False,)
+baseModel = MobileNetV2(input_shape=(256, 256, 3), weights='imagenet', include_top=False)
 
-headModel = baseModel.output
-headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
-headModel = Flatten(name="flatten")(headModel)
-headModel = Dense(128, activation="relu")(headModel)
-headModel = Dropout(0.5)(headModel)
-headModel = Dense(2, activation="softmax")(headModel)
-
-model = Model(inputs=baseModel.input, outputs=headModel)
-for layer in baseModel.layers:
-    layer.trainable = False
-
-print("[INFO] compiling model...")
-opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
-model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
-
-aug = ImageDataGenerator(
-    rotation_range=20,
-    zoom_range=0.15,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.15,
-    horizontal_flip=True,
-    fill_mode="nearest")
-
-print("[INFO] training head...")
-H = model.fit(
-    aug.flow(trainX, trainY, batch_size=BS),
-    steps_per_epoch=len(trainX) // BS,
-    validation_data=(testX, testY),
-    validation_steps=len(testX) // BS,
-    epochs=EPOCHS)
+# headModel = baseModel.output
+# headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+# headModel = Flatten(name="flatten")(headModel)
+# headModel = Dense(128, activation="relu")(headModel)
+# headModel = Dropout(0.5)(headModel)
+# headModel = Dense(2, activation="softmax")(headModel)
+#
+# model = Model(inputs=baseModel.input, outputs=headModel)
+# for layer in baseModel.layers:
+#     layer.trainable = False
+#
+# print("[INFO] compiling model...")
+# opt = Adam(lr=INIT_LR, decay=INIT_LR / EPOCHS)
+# model.compile(loss="binary_crossentropy", optimizer=opt, metrics=["accuracy"])
+#
+#
+# print("[INFO] training head...")
+# H = model.fit(
+#     aug.flow(trainX, trainY, batch_size=BS),
+#     steps_per_epoch=len(trainX) // BS,
+#     validation_data=(testX, testY),
+#     validation_steps=len(testX) // BS,
+#     epochs=EPOCHS)
