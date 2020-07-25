1dcf6372082d02f2dee18b70b3a75e54dd0047be
Jathin Pranav Singaraju <66273559+DaOne23-JPS@users.noreply.github.com>
2020-07-25 16:34:45 -0500

    Final Major Commit
    Renamed most of the files. Changed it to python convention. Commented what was useless and provided directions for convert_images.py and process_data.py. Still need to update the READ.md, will do that later.

--- convert_images.py
+++ convert_images.py
@@ -3,7 +3,10 @@ import os
 import cv2
 import time
 
-# this file converts images from 1024 x 1024, rgb, to 224 x 224, grayscale
+# this file converts images from 1024 x 1024, rgb, to 224 x 224, grayscale and saves them into files
+# in this case, this is already done, so no need to be executed because the images are already resized and filtered
+# just unzip then from the zipped file and place them into two different files
+# however, if you do want to run this yourself download the link for the original dataset mentioned in GitHub
 
 # Dataset Folder Structure
 # Face Mask Detection Data
@@ -13,51 +16,42 @@ import time
 #           |_ new_without_mask
 #           |_ new_with_mask
 
-# get the directory for the files
+# get the directory for the files, uncomment these lines below
 
-# Pranav Directories
-#
-# withoutDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/without_mask/'
-# withDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/with_mask/'
-# newWithoutDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/new_without_mask/'
-# newWithDir = 'C:/Users/Singaraju/Desktop/Face Mask Detection Data/20k_faces/new_with_mask/'
-
-# Lavik Directories
-
-withoutDir = 'D:/Face Mask Detection Dataset/without_mask/'
-withDir = 'D:/Face Mask Detection Dataset/with_mask/'
-newWithoutDir = 'D:/Face Mask Detection Dataset/new_without_mask/'
-newWithDir = 'D:/Face Mask Detection Dataset/new_with_mask/'
+# without_dir = your directory of original no mask dataset (1024 x 1024 rgb)
+# with_dir = your directory of original mask dataset (1024 x 1024 rgb)
+# new_without_dir = your new directory where updated no mask images will be saved (224 x 224 grayscale)
+# new_with_dir = your new directory where updated mask images will be saved (224 x 224 grayscale)
 
 # initialize the time variable
-totalTime = 0
+total_time = 0
 
 # set the folder you want to iterate through, in this case withDir (rgb images with masks)
-for image in os.listdir(withDir):
-    startTime = time.time()  # start the time
+for image in os.listdir(with_dir):
+    start_time = time.time()  # start the time
     print(image)  # print the image name
-    imageMain = cv2.imread(withDir + image)  # read the image from the directory specified
+    image_main = cv2.imread(with_dir + image)  # read the image from the directory specified
     dim = (224, 224)  # resize image to dim
-    imageMain = cv2.resize(imageMain, dim)  # resize the images with specs
-    imageMain = cv2.cvtColor(imageMain, cv2.COLOR_BGR2GRAY)  # make the image grayscale
-    cv2.imwrite(newWithDir + image, imageMain)  # write the image, or save it into the new folder (newWithDir)
-    endTime = time.time()  # end the time
-    totalTime += endTime - startTime  # add total time
-    print(totalTime)  # print the time after each iteration
+    image_main = cv2.resize(image_main, dim)  # resize the images with specs
+    image_main = cv2.cvtColor(image_main, cv2.COLOR_BGR2GRAY)  # make the image grayscale
+    cv2.imwrite(new_with_dir + image, image_main)  # write the image, or save it into the new folder (newWithDir)
+    end_time = time.time()  # end the time
+    total_time += end_time - start_time  # add total time
+    print(total_time)  # print the time after each iteration
 
 # the other folder to iterate through, withoutDir (rgb without masks), same code, different directory
-for image in os.listdir(withoutDir):
-    startTime = time.time()  # start the time
+for image in os.listdir(without_dir):
+    start_time = time.time()  # start the time
     print(image)  # print the image name
-    imageMain = cv2.imread(withoutDir + image)  # read the image from the directory specified
+    image_main = cv2.imread(without_dir + image)  # read the image from the directory specified
     dim = (224, 224)  # resize image to dim
-    imageMain = cv2.resize(imageMain, dim)  # resize the images with specs
-    imageMain = cv2.cvtColor(imageMain, cv2.COLOR_BGR2GRAY)  # make the image grayscale
-    cv2.imwrite(newWithoutDir + image, imageMain)  # write the image, or save it into the new folder (newWithDir)
-    endTime = time.time()  # end the time
-    totalTime += endTime - startTime  # add total time
-    print(totalTime)  # print the time after each iteration
+    image_main = cv2.resize(image_main, dim)  # resize the images with specs
+    image_main = cv2.cvtColor(image_main, cv2.COLOR_BGR2GRAY)  # make the image grayscale
+    cv2.imwrite(new_without_dir + image, image_main)  # write the image, or save it into the new folder (newWithDir)
+    end_time = time.time()  # end the time
+    total_time += end_time - start_time  # add total time
+    print(total_time)  # print the time after each iteration
 
-# code to show image with array = imageMain
-# plt.imshow(imageMain)
+# code to show image with array = image_main
+# plt.imshow(image_main)
 # plt.show()
--- progress_bar.py
+++ progress_bar.py
@@ -1,19 +1,26 @@
 # truncate the time decimal to 4 decimals
 def truncate(n, decimals=4):
+    # get the multiplier value
     multiplier = 10 ** decimals
+
+    # return the truncated time in specified decimals
     return int(n * multiplier) / multiplier
 
 
-# this is the bar method to print the updated bar with the specified params
-def barMethod1(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', printEnd="\r", time='0'):
-    # this is the amount of percent completed
+# this is the bar method to print the updated progress bar with the specified params
+def bar_method(iteration, total, prefix='', suffix='', decimals=1, length=100, fill='█', print_end="\r", time='0'):
+    # this is the calculation for the amount of percent completed
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    # fills the bar to the specified amount
-    filledLength = int(length * iteration / total)
-    # updates the bar
-    bar = fill * filledLength + '-' * (length - filledLength)
-    # prints the bar with the correct params
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}  >>>  time taken: {truncate(time)} secs', end=printEnd)
+
+    # fills the progress bar to the specified amount (filled_length)
+    filled_length = int(length * iteration / total)
+
+    # updates the progress bar
+    bar = fill * filled_length + '-' * (length - filled_length)
+
+    # prints the progress bar with the correct params
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}  >>>  time taken: {truncate(time)} secs', end=print_end)
+
     # if the bar is complete, print and stop
     if iteration == total:
         print()
